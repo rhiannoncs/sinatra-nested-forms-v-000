ecb290bb17fd7675e2ed2764983265da505152a4
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-03-04 02:39:05 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,9 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+
+  def initialize (params)
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
+  end
 end
